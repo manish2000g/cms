c157ce58d463295e8fe4367c763f8d4267c3413e
Manish Gautam <gautammanish700@gmail.com>
2023-08-08 11:42:08 +0545

    ff

--- CMS/urls.py
+++ CMS/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('account.urls')),
@@ -24,3 +27,7 @@ urlpatterns = [
     # path('api/',include('provider.urls')),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
